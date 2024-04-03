1b03b2b38fcf8854e304dd4c19f5b937f558a221
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2024-04-03 09:44:00 -0300

    Explicar técnicas utilizadas

--- TP2a_Operaciones_Puntuales/Ej4.1_objetos_no_visibles.py
+++ TP2a_Operaciones_Puntuales/Ej4.1_objetos_no_visibles.py
@@ -5,6 +5,18 @@ visibles con buen contraste sin realizar modicaciones sustanciales en el resto
 de la imagen.
 """
 
+"""
+Una opción que se implemento fue la ecualización del histograma, para tener un mejor
+contraste en la imagen y poder visualizar los objetos que no se percibian.
+
+Otra opción fue aplicar una transformación lineal con lo que vimos en el ejercicio 1
+usando una transformacion lineal cambiando el parámetro "a" para mejorar el contraste, pero
+no se logra mejorar mucho la visualización de los objetos.
+También se probé usar la imagen en el Ej2 junto con transformaciones no lineales, como la 
+logaritmica y la de potencia, con las cuales se observan los objetos no visibles pero se 
+altera demasiado la imagen original.
+"""
+
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import cv2
@@ -20,9 +32,8 @@ img_eq = cv2.equalizeHist(img)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 img_clahe = clahe.apply(img)
 
-#* Probar otras tecnicas
-# Probe usar la imagen en el Ej2 con las transformaciones no lineales (log y potencia) 
-# pero se altera demasiado la imagen original.
+# Se probo con otras opciones usando transformaciones como se menciono en el comentario arriba,
+# pero no se obtuvieron mejores resultados. 
 
 #* Muestro todas las imagenes obtenidas
 cv2.imshow('Imagen original', img)
